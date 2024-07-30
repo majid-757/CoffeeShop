6253d5190afac133b4ec36a06b9059df7feb9e59
Majid <majid.asadollahi95@gmail.com>
2024-07-31 03:05:48 +0330

    Add project files.

---
+++ CoffeeShop/Migrations/20240730220701_final1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class final1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/images/anna-bratiychuk-3w2aurzeesu-unsplash-815x1223.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/images/American.jpg");
+        }
+    }
+}
